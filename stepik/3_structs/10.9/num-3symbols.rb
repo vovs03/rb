5b40a49478218bee7aed61f6b5653a264eee08eb
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-04-28 11:55:47 +0300

    Style: clean

--- stepik/3_structs/10.9/num-3symbols.rb
+++ stepik/3_structs/10.9/num-3symbols.rb
@@ -1,15 +1,10 @@
 #!/usr/bin/env ruby
 # 2022-04-28 11:10
+# Repo: https://github.com/vovs03/rb
 # Stepik 3.10.9
 # end. Stop of finding best variant 11:47
 
-a = gets.split("")
-puts __LINE__
-
-p a
-puts __LINE__
-
-#n = 0
+a = gets.split('')
 
 for i in (0..2)
   a[i].to_i
@@ -17,6 +12,3 @@ end
 
 # Works |> But want write it nice
 puts (a[0].to_i + a[1].to_i + a[2].to_i)
-
-#puts z[0..2].sum
-#puts (a[0]+a[1]+a[2])
