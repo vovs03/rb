42cafbf9ee2ec626b6ff74cb29135134256ebcd6
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-04-29 12:50:47 +0300

    Add: Stepik 3_Structs.7.2 | Example until

---
+++ stepik/3_structs/7.2/example.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# Stepik
+# 2022-04-29 12:48
+# Repo: https://github.com/vovs03/rb
+
+x = 15
+until x <= 10
+  puts x
+  x -= 2
+end
+
+__END__
+Сколько раз будет выполнен код в этом цикле: # 3
