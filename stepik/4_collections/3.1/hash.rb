daef270e916eb2618e4fed4e9e21d3bf892d1e8c
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-05-01 12:35:33 +0300

    Fix: 📝 In range (22..24) use values of hsh

--- stepik/4_collections/3.1/hash.rb
+++ stepik/4_collections/3.1/hash.rb
@@ -2,12 +2,13 @@
 # 2022-04-30 21:26
 # Stepik 4.3.1
 # https://github.com/vovs03/rb
+# https://replit.com/@vovs03/gethshkeys#main.rb
+# UPD: 2022-05-01
 # Author: Vladimir Pavlychev
 
 winter = { 'dec' => 1, jan: 2, feb: 3 }
-
-#kv = { 'k' => 22, l: 23, m: 24, 'n' => 36 }
-kv = { ku: 22, la: 23, mi: 24 }
+kv = { 'k' => 22, l: 23, m: 24, 'n' => 36 }
+#kv = { ku: 22, la: 23, mi: 24 }
 
 p "--- 1 ---"
 for i in (1..3)
@@ -15,11 +16,13 @@ for i in (1..3)
 end
 
 p "--- 2 ---"
-# Here is output `nil`
+# Here is output `nil` if range (1..3) - It's not `index number of hsh.key(value)` 
 # Because (values) == (22..24)
-for _i in (1..3)
-  p kv.key(_i) #.inspect
+# That's result that me want to get
+for _i in (22..24)
+  p kv.key(_i) 
 end
 
 p "--- 3 ---"
-p *winter.each #{|x|}
+# decompose the hsh |> [..],[..],[..]
+p *winter.each
--- stepik/4_collections/README.md
+++ stepik/4_collections/README.md
@@ -10,4 +10,33 @@
 
 ## 4.3  Hashes & Symbols
 
+- [x] 3.1 [get hsh keys](https://replit.com/@vovs03/gethshkeys#main.rb)
+
+---
+
+### Examples
+
+```irb
+[66] pry(main)> ww = { [1, "jan"] => "January" }
+=> {[1, "jan"]=>"January"}
+[67] pry(main)> p ww.keys
+[[1, "jan"]]
+=> [[1, "jan"]]
+[68] pry(main)> p ww[[1, "jan"]]
+"January"
+=> "January"
+[69] pry(main)> p ww.key('January')
+[1, "jan"]
+=> [1, "jan"]
+```
+В этом примере хочу отметить, что 
+ - `67` > вернёт все ключи хэша
+ - `68` |> output `value` by `key`
+ - `69` |> output `key` by `value`
+
+### Summary
+
+- Было не понятно, как извлечь значение "January"?
+ - `{ [1, "jan"] => "January" }`
+
 - [x] 5.2 Итератор
