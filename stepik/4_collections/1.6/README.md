8c0164d8b2faa9724b3993d2e77d57541bfb3f3d
Vladimir Vic. Pavlychev <vladimir.keyjoo@gmail.com>
2022-05-13 16:41:31 +0300

    Refactor: 🚀 Stepik 4.1.6 Management array elements

---
+++ stepik/4_collections/1.6/README.md
@@ -0,0 +1,31 @@
+# Management array elements
+
+> Stepik 4.1.6
+
+```ruby
+# хотел сделать слайс для преобразования num(1..3).to_i
+# Знаю, что в рубокопе по стайлгайду не приветствуются иные языки кроме ASCII
+
+## start debugging
+# binding.pry
+```
+
+1. |>  arr.slice(1, 3).each {|i| arr.to_i}
+2. |> Book Simdyanov
+3. |> Want to ask into [igor simdyanov](https://github.com/igorsimdyanov/ruby/issues/new)
+4. |> G: stackoverflow convert array elements from strings to integers
+      -- https://stackoverflow.com/questions/33819239/convert-array-elements-from-string-to-integer
+
+## первичный ход рассуждения
+
+> был примерно таким...
+
+```ruby
+arr[0] << str
+
+arr << num1
+arr << num2
+arr << num3
+
+p (arr[3] + arr.pop)
+```
--- stepik/4_collections/1.6/arr.rb
+++ stepik/4_collections/1.6/arr.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-#!/usr/bin/env ruby
+
+# !/usr/bin/env ruby
 
 # 2022-05-12 19:34
 # 2022-05-13 15:45 UPD
@@ -8,45 +9,15 @@
 # https://replit.com/@vovs03/arrmngmtrb#main.rb 16:25
 # Author: Vladimir Pavlychev
 
-require 'pry'
-
 arr = []
-# num1, num2, num3, str = gets.chomp
-# num1 = gets.chomp
 
-num1, num2, num3, str = Array.new(4) { arr << gets.chomp }
-
-# Refactored by *Cop message: 
-# Useless assignment to variable - `num1`. Use `_` or `_num1` 
+# Refactored by *Cop message:
+# Useless assignment to variable - `num1`. Use `_` or `_num1`
 # as a variable name to indicate that it won't be used. (warning:Lint/UselessAssignment)
 _ = Array.new(4) { arr << gets.chomp }
 
-# arr.take(3).map!(&:to_i)
-
-# Был затык вызывал pry
-# print arr
-# binding.pry
-
 arr.insert(0, arr.last)
 arr.pop
-
-# хотел сделать слайс для преобразования num(1..3).to_i
-# Знаю, что в рубокопе по стайлгайду не приветствуются иные языки кроме ASCII
-## 1. |>  arr.slice(1, 3).each {|i| arr.to_i}
-## 2. |> Book Simdyanov
-## 3. |> Want to ask onto https://github.com/igorsimdyanov/ruby/issues/new
-## 4. |> G: stackoverflow convert array elements from strings to integers
-##       -- https://stackoverflow.com/questions/33819239/convert-array-elements-from-string-to-integer
-
-# arr[0] << str
-
-## start debugging
-# binding.pry
-# arr << num1
-# arr << num2
-# arr << num3
-
-# p (arr[3] + arr.pop)
 
 p arr[1]
 print arr
