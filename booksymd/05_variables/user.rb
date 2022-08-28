2e671b8146c12c68fd27269821291720b9acad1b
Vladimir Victor Pavlychev <vladimir.keyjoo@gmail.com>
2022-08-28 09:32:34 +0300

    refactor: delete 2double-methods | into method -name- put array-symbols

--- booksymd/05_variables/user.rb
+++ booksymd/05_variables/user.rb
@@ -8,22 +8,21 @@ class User
   def name(name)
     @name = name
   end
-
-  def mid_name(name)
-    @name = name
-  end
-
-  def last_name(name)
-    @name = name
-  end
 end
 
-# user = User.name('Alex')
 user = User.new
-user = [user.name(:Alex), user.mid_name(:Pobeditovich), user.name(:Rubynoff)]
 
-# puts user.name('Alex')
+# user = [user.name(:Alex), user.mid_name(:Pobeditovich), user.name(:Rubynoff)]
+user = user.name(%i[Alex Pobeditovich Rubynoff])
+
 puts user
 
 __END__
 1. Есть ли необходимость в уникальности присваеваемых имён переменным методов?
+(этот вопрос привёл меня к дальнейшим размышлениям и рефакторингу)
+- Когда копипастил, то не заметил, что не заменил вызываемый метод для вывода фамилии `name` -> `last_name`
+- Решил отрефакторить
+  - сначала в метод `name` передал три :Символа, убраз вызов 3х методов
+  - удалил методы-дубликаты, оставив только метод `name`
+2. Можно будет опробовать структуру, описывающую класс `User`
+3. Проверить на использование кириллицы в символах
